49b1d66e4fe53ecdb8ca1ff7d9c6f60c69f4ecdc
Max Martin <mmart180@my.centennialcollege.ca>
2021-04-06 01:05:33 -0400

    auth changes

--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -4,6 +4,7 @@ let router = express.Router();
 
 let mongoose = require("mongoose");
 let User = require("../models/user");
+let jsonwebtoken = require('jsonwebtoken')
 
 module.exports.displayList = (req, res, next) => {
   User.find((err, UserList) => {
@@ -23,16 +24,31 @@ module.exports.authenticateUser = (req, res, next) => {
       }
       else
       {
+
           auth = false
+          let user;
           userList.map(u => {
               if(u.username==req.body.username && u.password==req.body.password){
                   auth = true;
+                  user=u;
               }
           })
-          res.send({"message" : auth ? "User is autheticated" : "User isn't authenticated"});      
+  const pk = "6e843f3a5c16fa508d63cc4cb73a921be7f4edfaae7ab56162449b7049d0bbd2cc18e46330ce22260fe1719cfb705a1b9703c1f4ec6fbdc89125ce57f0747fd4"
+          const token = jsonwebtoken.sign(user?.toString()||'',pk);
+          res.send({"message" : auth ? "User is autheticated" : "User isn't authenticated",token});      
       }
   });
 }
+
+module.exports.verify = (req, res, next) => {
+
+const pk = "6e843f3a5c16fa508d63cc4cb73a921be7f4edfaae7ab56162449b7049d0bbd2cc18e46330ce22260fe1719cfb705a1b9703c1f4ec6fbdc89125ce57f0747fd4"
+        const jw= jwt.verify(authToken,pk)
+        console.log(jw)
+        return jw
+}
+
+
 
 //Controller action for processing create action
 module.exports.processCreate = (req, res, next) => {
